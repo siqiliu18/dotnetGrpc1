b7a1e2ed31c020abe75093024004fcbc897af768
Siqi Liu <siqiliu18@gmail.com>
2020-04-27 21:45:16 -0700

    first commit

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+using GrpcServer;
+
+namespace GrpcClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("begins: ");
+            var serverAddress = "http://localhost:6000";    // + ipaddress;
+                                                         // This switch must be set before creating the GrpcChannel/HttpClient.
+            AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+
+            var input = new HelloRequest { Name = "Leo" };
+            var channel = GrpcChannel.ForAddress(serverAddress);
+            var client = new Greeter.GreeterClient(channel);
+            var reply = await client.SayHelloAsync(input);
+            Console.WriteLine(reply.Message);
+            Console.ReadLine();
+        }
+    }
+}
